a9376cc19a9f79f67d6d464c936a667179b16fbb
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2021-01-06 13:28:02 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const mongodburi = "mongodb://jaiswalrohan:jaiswalrohan@cluster0-shard-00-00.wq1cd.mongodb.net:27017,cluster0-shard-00-01.wq1cd.mongodb.net:27017,cluster0-shard-00-02.wq1cd.mongodb.net:27017/MoviesCollection?ssl=true&replicaSet=atlas-yf9ewr-shard-0&authSource=admin&retryWrites=true&w=majority";
-
-export default mongodburi;
